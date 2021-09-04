dc2f8e016d5f0d61fcf1a76db4c2b40c68cab1a3
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-09-04 12:00:50 -0400

    finish some functionalities of snake games

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -8,13 +8,30 @@ function setup() {
   createCanvas(600, 600);
   s = new snake();
   frameRate(10);
-  food = createVector(random(width), random(height));
+  pickLocation();
+}
+
+function pickLocation() {
+  let cols = floor(width / scl);
+  let rows = floor(height / scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
 }
 
 function draw() {
   background(51);
+
+  if (s.eat(food)) {
+    pickLocation();
+  }
+  s.death();
   s.update();
   s.show();
+
 
   fill(255, 0, 100);
   rect(food.x, food.y, scl, scl);
