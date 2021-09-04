fef7e362bd61dc5dcbc9e477961536ace029e5e6
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-09-04 13:32:03 -0400

    fix returned snake

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,8 +1,10 @@
 /* eslint-disable linebreak-style */
 let s;
-let scl = 20;
+const scl = 20;
 
 let food;
+
+let direction = 'right';
 
 function setup() {
   createCanvas(600, 600);
@@ -12,8 +14,8 @@ function setup() {
 }
 
 function pickLocation() {
-  let cols = floor(width / scl);
-  let rows = floor(height / scl);
+  const cols = floor(width / scl);
+  const rows = floor(height / scl);
   food = createVector(floor(random(cols)), floor(random(rows)));
   food.mult(scl);
 }
@@ -28,19 +30,36 @@ function draw() {
   s.update();
   s.show();
 
-
   fill(255, 0, 100);
   rect(food.x, food.y, scl, scl);
 }
 
 function keyPressed() {
+  if (direction === 'right' && (keyCode === LEFT_ARROW)) {
+    s.dir(1, 0);
+    return;
+  } if (direction === 'left' && (keyCode === RIGHT_ARROW)) {
+    s.dir(-1, 0);
+    return;
+  } if (direction === 'up' && (keyCode === DOWN_ARROW)) {
+    s.dir(0, 1);
+    return;
+  } if (direction === 'down' && (keyCode === UP_ARROW)) {
+    s.dir(0, -1);
+    return;
+  }
+
   if (keyCode === UP_ARROW) {
+    direction = 'up';
     s.dir(0, -1);
   } else if (keyCode === DOWN_ARROW) {
+    direction = 'down';
     s.dir(0, 1);
   } else if (keyCode === RIGHT_ARROW) {
+    direction = 'right';
     s.dir(1, 0);
   } else if (keyCode === LEFT_ARROW) {
+    direction = 'left';
     s.dir(-1, 0);
   }
 }
